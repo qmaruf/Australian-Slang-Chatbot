1533a8bea6215ac31935e1bfe3917272e4f27d24
Quazi Marufur Rahman <maruf.csdu@gmail.com>
2023-07-09 22:07:10 +1000

    bot and readme file

---
+++ README.md
@@ -0,0 +1,31 @@
+# Australian Slang Chatbot
+
+This is a chatbot that answers using Australian slang. The chatbot, named "Brissy," is powered by OpenAI's language model and can provide responses in a fair dinkum Aussie manner. It's designed to engage in conversations about Australia, Aussie culture, and a wide range of topics while incorporating friendly and authentic Australian slang.
+
+## Dependencies
+The following dependencies are required to run the chatbot:
+
+* gradio
+* openai
+* dotenv
+
+To install the dependencies, you can use the following command:
+
+```bash
+pip install gradio openai-python dotenv
+```
+Please ensure you have the appropriate access credentials for OpenAI's API by setting the `OPENAI_API_KEY` environment variable or using a `.env `file.
+
+## Usage
+To use the chatbot, follow these steps:
+
+1. Import the required modules and libraries.
+2. Set up the get_template() function, which returns the template for the chatbot's responses. It includes a placeholder for history and human input.
+3. Implement the get_chain() function, which returns the chatbot chain configuration using the LLMChain from the langchain library. This includes setting up the language model, prompt template, verbosity, and conversation memory.
+4. Define the interface() function, which launches the chatbot interface using the gradio library. It sets up the chatbot, message textbox, and clear button. It also includes the logic for user input and bot responses.
+5. Execute the interface() function to start the chatbot interface.
+
+## Notes
+1. The chatbot utilizes the OpenAI language model with a temperature setting of 0.5, which controls the randomness of the generated responses.
+2. The ConversationBufferWindowMemory is used to store and manage conversation history, with a buffer size of 100.
+3. The chatbot interface is created using the gradio library, providing an interactive and user-friendly experience.
---
+++ aussie_bot.py
@@ -0,0 +1,84 @@
+"""
+A chatbot that will answer using australian slang
+"""
+import os
+import time
+
+import gradio as gr
+import openai
+from dotenv import load_dotenv
+from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def get_template() -> str:
+    """
+    Returns the template for the chatbot
+    """
+    template = """Brissy is a large language model trained by OpenAI.
+
+    Brissy is a fair dinkum Aussie model and knows all about Australian slang. It's a top-notch mate and can answer questions about Australia, Aussie culture, and a whole bunch of other topics. It always uses friendly slang and can chat like a true blue Aussie.
+
+    Reckon you can rewrite your response using Australian slang?
+
+    {history}
+    Human: {human_input}
+    Brissy:"""
+
+    return template
+
+
+def get_chain() -> LLMChain:
+    """
+    Returns the chatbot chain
+    """
+    prompt = PromptTemplate(
+        input_variables=['history', 'human_input'],
+        template=template
+    )
+
+    chatgpt_chain = LLMChain(
+        llm=OpenAI(temperature=0.5),
+        prompt=prompt,
+        verbose=True,
+        memory=ConversationBufferWindowMemory(k=100),
+    )
+    return chatgpt_chain
+
+
+def interface() -> None:
+    """
+    Launches the chatbot interface.
+    """
+    with gr.Blocks() as demo:
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button('Clear')
+
+        def user(user_message, history):
+            return '', history + [[user_message, None]]
+
+        def bot(history):
+            human_input = history[-1][0]
+            response = chatgpt_chain.predict(human_input=human_input)
+
+            history[-1][1] = ''
+            for character in response:
+                history[-1][1] += character
+                time.sleep(0.01)
+                yield history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+            bot, chatbot, chatbot
+        )
+        clear.click(lambda: None, None, chatbot, queue=False)
+
+    demo.queue()
+    demo.launch(share=True)
+
+
+if __name__ == '__main__':
+    interface()
