780b62ba0347b2dc50f22d7d02adafb09a3b2835
Quazi Marufur Rahman <maruf.csdu@gmail.com>
2023-07-11 22:09:27 +1000

    response will be within 4000 chars

--- aussie_bot.py
+++ aussie_bot.py
@@ -3,6 +3,7 @@ A chatbot that will answer using australian slang
 """
 import os
 import time
+
 import gradio as gr
 import openai
 from langchain import LLMChain, PromptTemplate
@@ -18,7 +19,7 @@ def get_template() -> str:
     """
     template = """Brissy is a large language model trained by OpenAI.
 
-    Brissy is a fair dinkum Aussie model and knows all about Australian slang. It's a top-notch mate and can answer questions about Australia, Aussie culture, and a whole bunch of other topics. It always uses friendly slang and can chat like a true blue Aussie. Brissy start answering every question differently.
+    Brissy is a fair dinkum Aussie model and knows all about Australian slang. It's a top-notch mate and can answer questions about Australia, Aussie culture, and a whole bunch of other topics. It always uses friendly slang and can chat like a true blue Aussie. Brissy start answering every question differently. Brissy will always answer every question within 4000 characters.
 
     Reckon you can rewrite your response using Australian slang?
 
@@ -72,10 +73,10 @@ def interface() -> None:
         def bot(history):
             try:
                 human_input = history[-1][0]
-                
+
                 if chatgpt_chain is None:
                     raise Exception('Chatbot not initialized')
-                
+
                 if len(human_input) < 512:
                     response = chatgpt_chain.predict(human_input=human_input)
                 else:
